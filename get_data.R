a92bee7061798283f86517a5e4ae268c1b8b9b92
Dabra56 <david.alexandre.brassard@gmail.com>
2022-12-19 15:19:11 -0500

    d

--- get_data.R
+++ get_data.R
@@ -2,6 +2,14 @@ library(tidyverse)
 library(cansim)
 
 
-wages_canada <- get_cansim_vector("v2133245")
+businesses <- get_cansim_vector("v1203704157")
 
-write.csv(x = wages_canada, file="wages.csv")
+businesses <- 
+  businesses %>% 
+    select(Date,val_norm) %>% 
+    rename(Active_Businesses = val_norm)
+
+write.csv(x = businesses, file="businesses.csv")
+
+
+
