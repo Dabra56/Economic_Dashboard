d11ed21a51da1da4a82e7f07b33962130c9152aa
Dabra56 <david.alexandre.brassard@gmail.com>
2022-12-21 09:48:31 -0500

    labor and inflation

--- get_data.R
+++ get_data.R
@@ -133,7 +133,7 @@ cpi <-
 
 write.csv(x = cpi, file="data/cpi.csv")
 
-# Province - economy 
+# ----------------------- ECONOMY TABLE ------------------
 
 vector_gdp <- c("v62787277",
                 "v62787378",
@@ -455,4 +455,293 @@ dataframe_economy <-
 write.csv(x = dataframe_economy, file="data/table_economy.csv")
 
 
+# ---------------------------- INFLATION TABLE -----------------------------
+
+cpi_all_items  <- get_cansim_vector("v41690973")
+
+tail =  tail(cpi_all_items$Date,n=1)
+
+cpi_all_items <- 
+  cpi_all_items %>% 
+      mutate(y_o_y = ((val_norm / lag(val_norm, n=12))-1)*100,
+             m_o_m = ((val_norm / lag(val_norm, n=1))-1)*100,
+             component = "Consumer price index (CPI)") %>% 
+      filter(Date==tail) %>% 
+      select(component, y_o_y,m_o_m)
+
+cpi_no_energy <- get_cansim_vector("v41691238")%>% 
+  mutate(y_o_y = ((val_norm / lag(val_norm, n=12))-1)*100,
+         m_o_m = ((val_norm / lag(val_norm, n=1))-1)*100,
+         component = "CPI without energy") %>% 
+  filter(Date==tail) %>% 
+  select(component, y_o_y,m_o_m)
+
+cpi_food <- get_cansim_vector("v41690974")%>% 
+  mutate(y_o_y = ((val_norm / lag(val_norm, n=12))-1)*100,
+         m_o_m = ((val_norm / lag(val_norm, n=1))-1)*100,
+         component = "Food") %>% 
+  filter(Date==tail) %>% 
+  select(component, y_o_y,m_o_m)
+
+cpi_shelter<- get_cansim_vector("v41691050")%>% 
+  mutate(y_o_y = ((val_norm / lag(val_norm, n=12))-1)*100,
+         m_o_m = ((val_norm / lag(val_norm, n=1))-1)*100,
+         component = "Shelter") %>% 
+  filter(Date==tail) %>% 
+  select(component, y_o_y,m_o_m)
+
+cpi_transport<- get_cansim_vector("v41691128")%>% 
+  mutate(y_o_y = ((val_norm / lag(val_norm, n=12))-1)*100,
+         m_o_m = ((val_norm / lag(val_norm, n=1))-1)*100,
+         component = "Transportation") %>% 
+  filter(Date==tail) %>% 
+  select(component, y_o_y,m_o_m)
+
+cpi_good<- get_cansim_vector("v41691222")%>% 
+  mutate(y_o_y = ((val_norm / lag(val_norm, n=12))-1)*100,
+         m_o_m = ((val_norm / lag(val_norm, n=1))-1)*100,
+         component = "Good") %>% 
+  filter(Date==tail) %>% 
+  select(component, y_o_y,m_o_m)
+
+cpi_services<- get_cansim_vector("v41691230")%>% 
+  mutate(y_o_y = ((val_norm / lag(val_norm, n=12))-1)*100,
+         m_o_m = ((val_norm / lag(val_norm, n=1))-1)*100,
+         component = "Services") %>% 
+  filter(Date==tail) %>% 
+  select(component, y_o_y,m_o_m)
+
+inflation_table <- bind_rows(cpi_all_items,cpi_no_energy,cpi_food, cpi_shelter,cpi_transport, cpi_good,cpi_services)
+
+colnames(inflation_table) <- c("","Year/Year","Month/Month")
+
+write.csv(x = inflation_table, file="data/table_inflation.csv")
+
+
+# ---------------------------- EMPLOYMENT TABLE -----------------------------
+
+# Total jobs
+
+vector_jobs <- c("v2062811",
+                     "v2063000",
+                     "v2063189",
+                     "v2063378",
+                     "v2063567",
+                     "v2063756",
+                     "v2063945",
+                     "v2064134",
+                     "v2064323",
+                     "v2064512",
+                     "v2064701")
+
+province_vector <- c("Canada",
+                     "Newfoundland and Labrador",
+                     "Prince Edward Island",
+                     "Nova Scotia",
+                     "New Brunswick",
+                     "Quebec",
+                     "Ontario",
+                     "Manitoba",
+                     "Saskatchewan",
+                     "Alberta",
+                     "British Colombia")
+
+dataframe_jobs <- data.frame()
+
+for (i in 1:length(vector_jobs)) {
+  
+ jobs <- get_cansim_vector(vector_jobs[i]) 
+  tail =  tail(jobs$Date,n=1)
+  
+  jobs <- 
+    jobs  %>% 
+    mutate(m_o_m = ((val_norm / lag(val_norm, n=1))-1)*100,
+           y_o_y = ((val_norm / lag(val_norm, n=12))-1)*100) %>% 
+    filter(Date==tail) %>% 
+    select(m_o_m,y_o_y) %>% bind_cols(province_vector[i]) 
+
+  
+  dataframe_jobs <- 
+    dataframe_jobs %>% 
+    bind_rows(jobs)
+  
+}
+
+
+dataframe_jobs <- 
+  dataframe_jobs %>% 
+  select(...3,m_o_m,y_o_y) 
+
+colnames(dataframe_jobs)<- c("provinces","Job variations ^M/M^","Job variations ^Y/Y^")
+  
+
+# Unemployment
+
+vector_unemployment <- c("v2062815",
+                 "v2063004",
+                 "v2063193",
+                 "v2063382",
+                 "v2063571",
+                 "v2063760",
+                 "v2063949",
+                 "v2064138",
+                 "v2064327",
+                 "v2064516",
+                 "v2064705")
+
+province_vector <- c("Canada",
+                     "Newfoundland and Labrador",
+                     "Prince Edward Island",
+                     "Nova Scotia",
+                     "New Brunswick",
+                     "Quebec",
+                     "Ontario",
+                     "Manitoba",
+                     "Saskatchewan",
+                     "Alberta",
+                     "British Colombia")
+
+dataframe_unemployment <- data.frame()
+
+for (i in 1:length(vector_unemployment)) {
+  
+  unemployment <- get_cansim_vector(vector_unemployment[i]) 
+  tail =  tail(unemployment$Date,n=1)
+  
+  unemployment <- 
+    unemployment   %>% 
+    filter(Date==tail) %>% 
+    select(val_norm) %>% bind_cols(province_vector[i]) 
+  
+  
+  dataframe_unemployment <- 
+    dataframe_unemployment %>% 
+    bind_rows(unemployment)
+  
+}
+
+colnames(dataframe_unemployment)[2] <- "provinces"
+dataframe_unemployment <- 
+  dataframe_unemployment %>% 
+  select(provinces,val_norm) 
+
+colnames(dataframe_unemployment)[2] <- "Unemployement rate"
+
+
+# Employment rate 25 - 54 
+
+
+vector_employment <- c("v2062952",
+                         "v2063141",
+                         "v2063330",
+                         "v2063519",
+                         "v2063708",
+                         "v2063897",
+                         "v2064086",
+                         "v2064275",
+                         "v2064464",
+                         "v2064653",
+                         "v2064842")
+
+province_vector <- c("Canada",
+                     "Newfoundland and Labrador",
+                     "Prince Edward Island",
+                     "Nova Scotia",
+                     "New Brunswick",
+                     "Quebec",
+                     "Ontario",
+                     "Manitoba",
+                     "Saskatchewan",
+                     "Alberta",
+                     "British Colombia")
+
+dataframe_employment <- data.frame()
+
+for (i in 1:length(vector_employment)) {
+  
+  employment <- get_cansim_vector(vector_employment[i]) 
+  tail =  tail(employment$Date,n=1)
+  
+  employment <- 
+    employment   %>% 
+    filter(Date==tail) %>% 
+    select(val_norm) %>% bind_cols(province_vector[i]) 
+  
+  
+  dataframe_employment <- 
+    dataframe_employment %>% 
+    bind_rows(employment)
+  
+}
+
+colnames(dataframe_employment)[2] <- "provinces"
+dataframe_employment <- 
+  dataframe_employment %>% 
+  select(provinces,val_norm) 
+
+colnames(dataframe_employment)[2] <- "Employement rate ^25-54 years old^"
+
+
+# Job vacancy rate
+
+vector_vacancy <- c("v1212389365",
+                       "v1212389368",
+                       "v1212389371",
+                       "v1212389374",
+                       "v1212389377",
+                       "v1212389380",
+                       "v1212389383",
+                       "v1212389386",
+                       "v1212389389",
+                       "v1212389392",
+                       "v1212389395")
+
+province_vector <- c("Canada",
+                     "Newfoundland and Labrador",
+                     "Prince Edward Island",
+                     "Nova Scotia",
+                     "New Brunswick",
+                     "Quebec",
+                     "Ontario",
+                     "Manitoba",
+                     "Saskatchewan",
+                     "Alberta",
+                     "British Colombia")
+
+dataframe_vacancy <- data.frame()
+
+for (i in 1:length(vector_vacancy)) {
+  
+  vacancy <- get_cansim_vector(vector_vacancy[i]) 
+  tail =  tail(vacancy$Date,n=1)
+  
+  vacancy <- 
+    vacancy   %>% 
+    filter(Date==tail) %>% 
+    select(val_norm) %>% bind_cols(province_vector[i]) 
+  
+  
+  dataframe_vacancy <- 
+    dataframe_vacancy %>% 
+    bind_rows(vacancy)
+  
+}
+
+colnames(dataframe_vacancy)[2] <- "provinces"
+dataframe_vacancy <- 
+  dataframe_vacancy %>% 
+  select(provinces,val_norm) 
+
+colnames(dataframe_vacancy)[2] <- "Job vacancy rate"
+
+
+# MERGE
+
+
+dataframe_labor <- merge(x=dataframe_jobs,y=dataframe_unemployment, by = "provinces")
+dataframe_labor <- merge(x=dataframe_labor,y=dataframe_employment, by = "provinces")
+dataframe_labor <- merge(x=dataframe_labor,y=dataframe_vacancy, by = "provinces")
+
+
+write.csv(x = dataframe_labor, file="data/table_labor.csv")
 
