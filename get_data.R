4449a9a6462db57f3466822135f9174b341eac00
Dabra56 <david.alexandre.brassard@gmail.com>
2024-06-19 09:47:08 -0400

    may LFS

--- get_data.R
+++ get_data.R
@@ -2578,7 +2578,7 @@ fr_wealth_df <-
 
 
 
-files <- c("lfs/pub0324.csv","lfs/pub0224.csv","lfs/pub0323.csv") # Change for last month, last year
+files <- c("lfs/pub0524.csv","lfs/pub0424.csv","lfs/pub0523.csv") # Change for last month, last year
 
 NSNE_Young_People <- data.frame()
 
